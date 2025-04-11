711eb4029289d11f07a7085572cb085faa412ae2
samwellsTU <111082260+samwellsTU@users.noreply.github.com>
2025-04-11 13:55:44 -0400

    4/11 inclass

--- reverb.js
+++ reverb.js
@@ -1,24 +1,13 @@
 // Create the AudioContext, which is the main Web Audio environment
 const audioContext = new AudioContext();
 
-
-
-// Create a GainNode to control the feedback amount
-const feedback = audioContext.createGain();
-feedback.gain.value = 0.4; // Set feedback level (40% of signal loops back)
-
 // Create a GainNode to control the final output volume
 const outputGain = audioContext.createGain();
-outputGain.gain.value = 0.5; // Set output volume to 50% -6dBFS
-
-
+outputGain.gain.value = 1.0; // Set output volume to 50% -6dBFS
 
 // Connect the delayed signal to the output gain (and then to the speakers)
 
 outputGain.connect(audioContext.destination);
-
-
-
 
 // Variable to hold the microphone input node
 let mic;
@@ -27,37 +16,43 @@ let reverb;
 
 //CREATE CONVOLVER NODE HERE!!!!!!!!!!!!!!!!!!!!
 
+const createReverb = async function () {
+  let convolver = audioContext.createConvolver();
+
+  //load impulse response
+  let response = await fetch("ir/chopin-rev.wav");
+  let arraybuf = await response.arrayBuffer();
+  convolver.buffer = await audioContext.decodeAudioData(arraybuf);
+
+  return convolver;
+};
+
+reverb = await createReverb();
+reverb.connect(outputGain);
 
 // Function to start the microphone and connect it to the delay line
-const startMic = async function() {
-    // Resume the AudioContext (required by most browsers on user interaction)
-    await audioContext.resume();
+const startMic = async function () {
+  // Resume the AudioContext (required by most browsers on user interaction)
+  await audioContext.resume();
 
-    // Request access to the user's microphone
-    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+  // Request access to the user's microphone
+  const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
 
-    // Create a MediaStreamAudioSourceNode from the microphone stream
-    mic = audioContext.createMediaStreamSource(stream);
+  // Create a MediaStreamAudioSourceNode from the microphone stream
+  mic = audioContext.createMediaStreamSource(stream);
 
-    // Connect the microphone to the delay line (which is already connected to output)
-    mic.connect(reverb);
+  // Connect the microphone to the delay line (which is already connected to output)
+  mic.connect(reverb);
 
-    console.log('Audio started');
+  console.log("Audio started");
 };
 
 // Function to stop the microphone processing
-const stopMic = function() {
-    // Disconnect the microphone input from the audio graph
-    if (mic) mic.disconnect();
+const stopMic = function () {
+  // Disconnect the microphone input from the audio graph
+  if (mic) mic.disconnect();
 };
 
-
-
-
-
-
-
-
 // Set up event listeners for the Start and Stop buttons
-document.getElementById('start').addEventListener("click", startMic);
-document.getElementById('stop').addEventListener("click", stopMic);
+document.getElementById("start").addEventListener("click", startMic);
+document.getElementById("stop").addEventListener("click", stopMic);
